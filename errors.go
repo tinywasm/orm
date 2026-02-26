d89ed28763508b8b3b23b12a92f2302f1287ed5f
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-02-26 18:19:39 -0300

    to jules update docs

--- errors.go
+++ errors.go
@@ -1,15 +1,15 @@
 package orm
 
-import "errors"
+import "github.com/tinywasm/fmt"
 
 // ErrNotFound is returned when ReadOne() finds no matching row.
-var ErrNotFound = errors.New("record not found")
+var ErrNotFound = fmt.Err("record", "not", "found")
 
 // ErrValidation is returned when validate() finds a mismatch.
-var ErrValidation = errors.New("validation error")
+var ErrValidation = fmt.Err("error", "validation")
 
 // ErrEmptyTable is returned when TableName() returns an empty string.
-var ErrEmptyTable = errors.New("empty table name")
+var ErrEmptyTable = fmt.Err("name", "table", "empty")
 
 // ErrNoTxSupport is returned by DB.Tx() when the adapter does not implement TxAdapter.
-var ErrNoTxSupport = errors.New("transaction not supported")
+var ErrNoTxSupport = fmt.Err("transaction", "not", "supported")
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tinywasm/orm
 
 go 1.25.2
 
-require github.com/tinywasm/fmt v0.18.5 // indirect
+require github.com/tinywasm/fmt v0.18.5
